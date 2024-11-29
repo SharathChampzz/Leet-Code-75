ef7991f4f0e3ea0b876d5919cfd990c6dfbaad34
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-11-29 09:54:11 +0530

    Time: 523 ms (13.50%) | Memory: 31.7 MB (5.26%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,5 +1,33 @@
 class Solution:
+
     def maxOperations(self, nums: List[int], k: int) -> int:
+        """
+            We need to find pair-sum, So it is like two sum problem, where we can levarage compliment to find missing number
+
+            This problem can be solved using two approach:
+                #1. Two pointer -- For this you will have to sort the array, so time complexity increases
+                #2. Using compliment sum -- For this you will have to maintain mapping which increases memory complexity
+        """
+        count_mapping = defaultdict(int)
+        op_count = 0 # result operation count
+
+        for num in nums:
+            compliment = k - num
+
+            if count_mapping[compliment]: # if compliment exist and it should not be zero
+                count_mapping[compliment] -= 1
+                op_count += 1 # remove compliment and dont add the new number
+            else:
+                count_mapping[num] += 1
+
+        return op_count
+
+    def maxOperationsTwoPointer(self, nums: List[int], k: int) -> int:
+        """
+            This is solved using two pointer approach, where we need to sort first.
+            Sorting supposed to increase a time complexity -- O(nLogn)
+            But it uses constant memory space O(1)
+        """
         n = len(nums)
         left, right = 0, n-1
         op_count = 0
@@ -17,28 +45,5 @@ class Solution:
                 right -= 1
             else:
                 left += 1
-
-        return op_count
-
-
-    def maxOperationsTwo(self, nums: List[int], k: int) -> int:
-        """
-            We need to find pair-sum, So it is like two sum problem, where we can levarage compliment to find missing number
-
-            This problem can be solved using two approach:
-                #1. Two pointer -- For this you will have to sort the array.
-                #2. Using compliment sum -- Current problem is solved using it.
-        """
-        count_mapping = defaultdict(int)
-        op_count = 0 # result operation count
-
-        for num in nums:
-            compliment = k - num
-
-            if count_mapping[compliment]: # if compliment exist and it should not be zero
-                count_mapping[compliment] -= 1
-                op_count += 1 # remove compliment and dont add the new number
-            else:
-                count_mapping[num] += 1
 
         return op_count
