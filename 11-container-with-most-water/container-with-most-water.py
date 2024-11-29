8d86e676d443d30e23d87b766ef433068761a922
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-11-29 08:44:13 +0530

    Time: 100 ms (53.04%) | Memory: 27.9 MB (35.50%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,14 +1,13 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         """
-        Logic:
-            Area = Height * Breadth 
-            To get maximum area, we need vertical lines of maximum height and maximum distance (breadth)
+        Area = Height * Breadth 
+        To get maximum area, we need vertical lines of maximum height and maximum distance (breadth)
 
-            Looking at the diagram and as we need to find range of index, We can try Two Pointer
+        Looking at the diagram and as we need to find range of index, We can try Two Pointer
 
-            To get maximum height, we however need to iterate through all the heights. 
-            But atleast we can try considering maximum breadth by placing the pointers at the corners and move inside as we progress
+        To get maximum height, we however need to iterate through all the heights. 
+        But atleast we can try considering maximum breadth by placing the pointers at the corners and move inside as we progress
         """
         n = len(height)
         result = 0
@@ -18,6 +17,11 @@ class Solution:
         while left < right:
             current_capacity = min(height[left], height[right]) * (right - left)
             result = max(result, current_capacity)
+
+            # To achieve the maximum result, move the pointer with the smaller height.
+            # Moving the pointer with the greater height is ineffective
+            # because we need to increase min(height[left], height[right]).
+            # If we move the pointer with the greater height, we will still get the same old height or even less.
 
             if height[left] < height[right]:
                 left += 1
