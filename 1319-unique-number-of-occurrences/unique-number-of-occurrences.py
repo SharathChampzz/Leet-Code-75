64a49d50f10f861047e85ad2506ce286652063ef
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-11-30 14:19:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.40%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,9 +1,6 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        mapping = defaultdict(int)
-
-        for num in arr:
-            mapping[num] += 1 # build mapping
+        mapping = Counter(arr)
 
         return len(mapping) == len(set(mapping.values()))
         
