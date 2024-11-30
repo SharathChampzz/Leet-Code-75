12f43a5b4b5869f2a2b4d5f693dcf2e51f852514
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-11-30 13:56:34 +0530

    Time: 7 ms (58.92%) | Memory: 18.3 MB (6.00%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -3,10 +3,18 @@ class Solution:
         total_sum = sum(nums)
         left_sum = 0
 
-        for i, num in enumerate(nums):
-            if left_sum == (total_sum - num - left_sum):
+        """
+            total_sum = left_sum + pivot + right_sum
+            total_sum = left_sum + pivot + left_sum # because left_sum == right_sum
+            total_sum = 2*left_sum + pivot
+            left_sum = (total_sum - num) // 2
+            Let's calculate different left_sum and check where it leads to the current expression.
+        """
+
+        for i, pivot in enumerate(nums):
+            if left_sum == (total_sum - pivot) / 2:
                 return i
-            left_sum += num
+            left_sum += pivot
 
         return -1
         
