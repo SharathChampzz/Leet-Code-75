1d501e1d2d91ba090d1116722240666cdc7f0b28
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-11-30 08:51:52 +0530

    Time: 68 ms (46.53%) | Memory: 17.9 MB (5.02%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,9 +1,37 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         left = 0
+        zero_count = 0
+        max_len = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
+
+
+
+
+
+
+
+
+
+
+
+    def longestOnesMod(self, nums: List[int], k: int) -> int:
+        left = 0
         flip_rem = k # flips remaining count
 
-        max_len = 0
+        max_len = 0 # init result
 
         for right in range(len(nums)):
             if nums[right] == 0 and flip_rem > 0:
@@ -12,10 +40,8 @@ class Solution:
                 while nums[left] == 1 and left < len(nums):
                     left += 1 # move the pointer till zero
                 left += 1 # skip the current zero
-                # flip_rem += 1 # as we moved left pointer to next of first zero
 
             max_len = max(max_len, right - left + 1)
-            # print(f'max_len: {max_len} for left: {left} and right: {right} => flip_rem: {flip_rem}')
 
         return max_len
 
