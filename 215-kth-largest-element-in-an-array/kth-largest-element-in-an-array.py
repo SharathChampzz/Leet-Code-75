5e8a6724c3380a12ebada5674e099c57767df80c
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-11-30 17:23:07 +0530

    Time: 94 ms (33.81%) | Memory: 26.2 MB (97.91%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        """Solved using Max Heap"""
         for i in range(len(nums)):
             nums[i] *= -1 # multiply by -1 to build max heap
 
@@ -12,6 +13,7 @@ class Solution:
         return -1 * heapq.heappop(nums)
 
     def findKthLargestUsingMinHeap(self, nums: List[int], k: int) -> int:
+        """Solved using Min Heap"""
         heapq.heapify(nums)
 
         while len(nums) != k:
