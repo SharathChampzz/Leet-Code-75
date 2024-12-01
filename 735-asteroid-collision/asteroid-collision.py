eac1fffffc9ba9f20d1fdb0a8d23c17a40b7062f
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-01 09:10:12 +0530

    Time: 4 ms (77.75%) | Memory: 18.5 MB (5.32%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,5 +1,11 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        """
+        Asteroids will collide only when a positive element encounters a new negative element.
+        If there is already a negative element and a new positive element comes in, they will not collide as they are moving in different directions.
+
+        To simplify, we will focus on when negative elements come in and analyze all the positive elements present at the end.
+        """
         stack = []
 
         for asteroid in asteroids:
@@ -7,16 +13,14 @@ class Solution:
                 stack.append(asteroid)
             else:
                 while stack and stack[-1] > 0:
-                    if abs(stack[-1]) > abs(asteroid):
+                    if abs(stack[-1]) > abs(asteroid):  # New asteroid is smaller, so it will explode
                         break
-                    elif abs(stack[-1]) == abs(asteroid):
+                    elif abs(stack[-1]) == abs(asteroid):  # If same size, both explode
                         stack.pop()
                         break
                     else:
-                        stack.pop()
+                        stack.pop()  # New asteroid is bigger
                 else:
                     stack.append(asteroid)
 
         return stack
-
-        
