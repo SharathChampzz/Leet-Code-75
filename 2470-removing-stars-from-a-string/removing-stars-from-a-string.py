311abc29c5cb15de93ff39d0e29fcf459115a35c
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-01 08:46:44 +0530

    Time: 91 ms (60.46%) | Memory: 18.5 MB (8.04%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -1,10 +1,13 @@
 class Solution:
     def removeStars(self, s: str) -> str:
+        # If the problem involves performing an operation on the previous index based on a certain condition,
+        # consider using a stack data structure to help solve the problem.
+
         stack = []
 
         for char in s:
             if char == '*' and stack:
-                stack.pop()
+                stack.pop() 
             else:
                 stack.append(char)
 
