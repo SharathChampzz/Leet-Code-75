16e73b83dba68a5a0a2508efb54044cd66c47c42
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-01 13:09:48 +0530

    Time: 27 ms (90.44%) | Memory: 21.9 MB (61.22%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -1,13 +1,13 @@
 class RecentCounter:
 
     def __init__(self):
-        self.counter = deque([])
+        self.counter = deque([]) # using dequeue, it is easy to add new elements and remove older unwanted elements
 
     def ping(self, t: int) -> int:
         self.counter.append(t)
 
         while (t - self.counter[0]) > 3000:
-            self.counter.popleft()
+            self.counter.popleft() # remove all the requests that are older than 3000
 
         return len(self.counter)       
 
