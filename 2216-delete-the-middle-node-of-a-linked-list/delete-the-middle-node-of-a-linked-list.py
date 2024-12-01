f6d4e99ffae6d37d5f3d59bae05dec6a839515ef
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-01 19:52:58 +0530

    Time: 58 ms (50.19%) | Memory: 48 MB (42.71%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -5,6 +5,14 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+            This can be solved using the slow and fast pointer technique because the middle element can be identified using this method.
+            The logic is that if you move the fast pointer two steps and the slow pointer one step, the fast pointer will reach the end first.
+            At that exact time, the slow pointer will be pointing to the middle of the list.
+        """
+        if not head or not head.next:
+            return None
+
         self.slow_p = self.fast_p = head
         self.prev = None
 
@@ -13,10 +21,7 @@ class Solution:
             self.slow_p = self.slow_p.next
             self.fast_p = self.fast_p.next.next
 
-        if self.prev:
-            self.prev.next = self.slow_p.next
-        else:
-            return None
+        self.prev.next = self.slow_p.next
 
         return head
         
