13975598bd3948f6c91616408da3378303cb2dec
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-02 23:10:10 +0530

    Time: 159 ms (7.84%) | Memory: 31.5 MB (9.93%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,19 +6,23 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+        # keep track of bigger number during traversal
+        # if current_val is smaller than already visited bigger number it means it is not good node
+        # if current_val is equal or greather than already visited numbers or that big number. It can be considered as good node
+        # So to calculate, you will left good nodes, right good nodes and you should also consider the current node
 
-        def search_node(node, current_max):
+        # Key point in tree problems is, you need to break down the big problem into root, left and right and start analysing it.
+
+        def get_good_nodes_count(node, current_max):
             if not node:
                 return 0
 
             new_max = max(current_max, node.val)
             
-            left_nodes = search_node(node.left, new_max)
-            right_nodes = search_node(node.right, new_max)
+            left_nodes  = get_good_nodes_count(node.left, new_max)
+            right_nodes = get_good_nodes_count(node.right, new_max)
 
-            return left_nodes + right_nodes + (1 if node.val >= current_max else 0)
+            return left_nodes + right_nodes + (1 if node.val >= current_max else 0) # left + right + current_root
 
-        answer = search_node(root, root.val)
-
-        return answer
+        return get_good_nodes_count(root, root.val)
         
