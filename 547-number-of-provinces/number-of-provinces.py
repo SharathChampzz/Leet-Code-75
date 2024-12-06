6f9a0ea275b4a8b0696c1f7bef11c68b72070efb
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-06 08:17:18 +0530

    Time: 8 ms (37.49%) | Memory: 18.9 MB (5.27%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,25 +1,24 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        visited = set() # stores i
+        visited = set()  # Set to store visited nodes (cities)
 
-        m, n = len(isConnected), len(isConnected[0])
+        m, n = len(isConnected), len(isConnected[0])  # Number of cities (m == n)
 
-        def dfs(i):
+        def dfs(i: int):
             if i in visited:
                 return
 
-            visited.add(i)
+            visited.add(i)  # Mark the current city as visited
 
             for j in range(n):
-                if isConnected[i][j] == 1:
-                    dfs(j)
-           
+                if isConnected[i][j] == 1:  # If there is a direct connection
+                    dfs(j)  # Recursively visit the connected city
 
-        count = 0 # connected provinces count
+        count = 0  # Counter for the number of provinces
 
         for i in range(m):
             if i not in visited:
-                dfs(i)
-                count += 1
-            
+                dfs(i)  # Start a DFS from the unvisited city
+                count += 1  # Increment the province count
+
         return count
