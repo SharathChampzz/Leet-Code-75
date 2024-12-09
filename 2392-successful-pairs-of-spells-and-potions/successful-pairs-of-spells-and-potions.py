88f35f2240f75d216c1daa320ab26d7f03760969
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-09 09:24:40 +0530

    Time: 503 ms (52.73%) | Memory: 37.5 MB (69.69%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -1,25 +1,21 @@
+
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        m, n = len(spells), len(potions)
-        # spells.sort()
+        # Sort potions to use binary search
         potions.sort()
         result = []
 
-        for i in range(m):
-            current_spell = spells[i]
-            left, right = 0, n-1
+        for spell in spells:
+            # Binary search to find the first potion that forms a successful pair
+            left, right = 0, len(potions)
             while left < right:
                 mid = (left + right) // 2
-                prod = current_spell * potions[mid]
-
-                if prod >= success:
+                if spell * potions[mid] >= success:
                     right = mid
                 else:
                     left = mid + 1
 
-            result.append(n-left) if (current_spell * potions[left]) >= success else result.append(0)
+            # The number of successful pairs for the current spell
+            result.append(len(potions) - left)
 
         return result
-
-
-        
