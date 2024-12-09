ad34c76af6c0c3a96a2cad37ef0bbb48842ce3dc
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-09 09:35:01 +0530

    Time: 4 ms (5.56%) | Memory: 17.6 MB (6.18%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,8 +1,6 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-
-        s = sorted(enumerate(nums), key= lambda x: x[1])
-        # print(s)
-
-        return s[-1][0]
+        # sort and get the last element
+        # but we need to return index so enumerate (index, val) and then sort based on val
+        return sorted(enumerate(nums), key= lambda x: x[1])[-1][0] # return last element and index
         
