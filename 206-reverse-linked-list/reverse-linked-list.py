6d83661958227dce4e08fe51627c7b4c9854da18
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-15 10:46:50 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (11.02%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -5,14 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev_node = None
-        current_node = head
-        temp = None
+        prev = None
+        current = head
 
-        while current_node:
-            temp = current_node.next 
-            current_node.next = prev_node
-            prev_node = current_node
-            current_node = temp
+        while current:
+            temp = current.next # store 5
+            current.next = prev # point 4 to 3
+            prev = current
+            current = temp # move current to 5
         
-        return prev_node
+        return prev
