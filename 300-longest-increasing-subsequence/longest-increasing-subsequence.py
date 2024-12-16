234da0ab8f41c471cd1f6e206c95466b5c696094
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-12-16 11:12:02 +0530

    Time: 1844 ms (27.71%) | Memory: 17.8 MB (11.97%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -2,19 +2,22 @@ from typing import List
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [1] * n
+        if not nums:
+            return 0
 
-        for i in range(n - 2, -1, -1):
-            for j in range(i+1, n):
-                if nums[j] > nums[i]:
+        # Step 1: Initialize DP array
+        dp = [1] * len(nums)
+
+        # Step 2: Fill DP array
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
                     dp[i] = max(dp[i], dp[j] + 1)
-                    # break
 
-        print(dp)
+        # Step 3: Find the maximum length
         return max(dp)
-        
-
+       
+# solving using backtracking
 class BackTrackingSolution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         def backtrack(idx, prev):
